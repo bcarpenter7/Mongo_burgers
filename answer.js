ca10edd1e951fc239b31a0f821a54a9fb90d20dc
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-06-15 14:00:39 -0400

    halfway

--- answer.js
+++ answer.js
@@ -1,25 +1,159 @@
 // create 5 burgers (at least 3 should be beef)
-
-
+db.burger.insert({meat: 'beef', cheese: 'false', toppings: ['ketchup', 'onions', 'pickles']})
+db.burger.insert({meat: 'beef', cheese: 'false', toppings: ['ketchup', 'onions', 'pickles']})
+db.burger.insert({meat: 'beef', cheese: 'pepperjack', toppings: ['onions', 'pickles']})
+db.burger.insert({meat: 'turkey', cheese: 'false', toppings: ['tomato']})
+db.burger.insert({meat: 'bean', cheese: 'queso', toppings: ['lettuce', 'tomato', 'cilantro']})
 // find all the burgers
-
+// Atlas atlas-3x9ncu-shard-0 [primary] burgers> db.burger.find()
+[
+  {
+    _id: ObjectId("648b361a7d28536a54c61af6"),
+    meat: 'beef',
+    cheese: 'false',
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b366d7d28536a54c61af7"),
+    meat: 'beef',
+    cheese: 'false',
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b37177d28536a54c61af8"),
+    meat: 'beef',
+    cheese: 'pepperjack',
+    toppings: [ 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b37647d28536a54c61af9"),
+    meat: 'turkey',
+    cheese: 'false',
+    toppings: [ 'tomato' ]
+  },
+  {
+    _id: ObjectId("648b379a7d28536a54c61afa"),
+    meat: 'bean',
+    cheese: 'queso',
+    toppings: [ 'lettuce', 'tomato', 'cilantro' ]
+  }
+]
 
 // show just the meat of each burger
+db.burger.find({}, {meat: 1})
+[
+  { _id: ObjectId("648b361a7d28536a54c61af6"), meat: 'beef' },
+  { _id: ObjectId("648b366d7d28536a54c61af7"), meat: 'beef' },
+  { _id: ObjectId("648b37177d28536a54c61af8"), meat: 'beef' },
+  { _id: ObjectId("648b37647d28536a54c61af9"), meat: 'turkey' },
+  { _id: ObjectId("648b379a7d28536a54c61afa"), meat: 'bean' }
+]
+
+
 
 
 // show just the toppings of each burger
 
 
+db.burger.find({}, {toppings: 1})
+[
+  {
+    _id: ObjectId("648b361a7d28536a54c61af6"),
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b366d7d28536a54c61af7"),
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b37177d28536a54c61af8"),
+    toppings: [ 'onions', 'pickles' ]
+  },
+  { _id: ObjectId("648b37647d28536a54c61af9"), toppings: [ 'tomato' ] },
+  {
+    _id: ObjectId("648b379a7d28536a54c61afa"),
+    toppings: [ 'lettuce', 'tomato', 'cilantro' ]
+  }
+]
+
 // show everything but the cheese
+
+db.burger.find({}, {cheese: 0})
+[
+  {
+    _id: ObjectId("648b361a7d28536a54c61af6"),
+    meat: 'beef',
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b366d7d28536a54c61af7"),
+    meat: 'beef',
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b37177d28536a54c61af8"),
+    meat: 'beef',
+    toppings: [ 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b37647d28536a54c61af9"),
+    meat: 'turkey',
+    toppings: [ 'tomato' ]
+  },
+  {
+    _id: ObjectId("648b379a7d28536a54c61afa"),
+    meat: 'bean',
+    toppings: [ 'lettuce', 'tomato', 'cilantro' ]
+  }
+]
 
 
 // find all the burgers with beef
 
+db.burger.find({meat: 'beef'})
+[
+  {
+    _id: ObjectId("648b361a7d28536a54c61af6"),
+    meat: 'beef',
+    cheese: 'false',
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b366d7d28536a54c61af7"),
+    meat: 'beef',
+    cheese: 'false',
+    toppings: [ 'ketchup', 'onions', 'pickles' ]
+  },
+  {
+    _id: ObjectId("648b37177d28536a54c61af8"),
+    meat: 'beef',
+    cheese: 'pepperjack',
+    toppings: [ 'onions', 'pickles' ]
+  }
+]
+
 
 // find all the burgers that are not beef
 
+db.burger.find( {meat: {$ne: 'beef'}})
+[
+  {
+    _id: ObjectId("648b37647d28536a54c61af9"),
+    meat: 'turkey',
+    cheese: 'false',
+    toppings: [ 'tomato' ]
+  },
+  {
+    _id: ObjectId("648b379a7d28536a54c61afa"),
+    meat: 'bean',
+    cheese: 'queso',
+    toppings: [ 'lettuce', 'tomato', 'cilantro' ]
+  }
+]
+
 
 // find the first burger with cheese
+
 
 
 // find one and update the first burger with cheese to have a property of 'double cheese'
